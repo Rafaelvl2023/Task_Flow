ce9ad0a12cf2a650d83246729574c97b5aef35d8
Rafael Teixeira <rafaelteixeiravl@gmail.com>
2025-04-21 13:31:38 -0300

    feat: cria models para project e task

---
+++ app/Models/Task.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'status', 'due_date', 'project_id'];
+
+    protected $casts = [
+        'due_date' => 'date',
+    ];
+
+    /**
+     * Relacionamento com o Projeto
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
