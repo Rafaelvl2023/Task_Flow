57c5d051b3d6d89ca59b4b3a433b06ea7ac83703
Rafael Teixeira <rafaelteixeiravl@gmail.com>
2025-04-21 13:14:51 -0300

    feat: define as rotas para dashboard, projects e tasks

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('projects', ProjectController::class);
+
+Route::resource('tasks', TaskController::class);
